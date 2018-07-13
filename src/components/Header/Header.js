37c5fd0bd8031fa909a9d4dcbf91538c1ea5061a
Dilwoar Hussain <dilwoar.hussain@digital.cabinet-office.gov.uk>
2018-07-13 17:00:15 +0100

    home page updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,7 +12,7 @@ class Header extends Component {
             <a href="#" className="govuk-header__link govuk-header__link--homepage">
               <span className="govuk-header__logotype">
                 <span className="govuk-header__logotype-text">
-                  #TeamCAAP
+                  Charity as a platform
                 </span>
               </span>
             </a>
--- src/views/Homepage/Homepage.js
+++ src/views/Homepage/Homepage.js
@@ -1,12 +1,54 @@
 import React, { Component } from 'react';
 import Header from '../../components/Header/Header';
 import './Homepage.css';
+import crossPlatformLogo from './cross-platform.svg';
 
 
 class Homepage extends Component {
   render() {
     return (
-      <Header />
+      
+      <div className="homepage">
+        <Header />
+
+        <div className="banner">
+          <div className="govuk-width-container">
+            <div class="govuk-grid-row">
+              <div className="govuk-grid-column-two-thirds">
+                <h1 className="govuk-heading-xl">
+                  Build it once then share it with everyone.
+                </h1>
+                <p className="govuk-body-l">
+                  Replicate good practice, reduce wasted cost.
+                </p>
+              </div>
+              <div className="govuk-grid-column-one-third">
+                <img src={crossPlatformLogo} />
+              </div>
+            </div> 
+          </div>
+        </div>
+
+        <div class="govuk-width-container">
+          <main class="govuk-main-wrapper " id="main-content" role="main">
+            <div class="govuk-grid-row">
+              <div class="govuk-grid-column-two-thirds">
+                <h1 className="govuk-heading-xl">
+                  Welcome
+                </h1>                
+                <p className="govuk-body-l">
+                  Charity as a Platform is a new way of building digital services. We’re creating a set of shared components, service designs, platforms, data and hosting, that every charity service can use.
+                </p>
+                <p className="govuk-body-l">
+                  This saves time and money for charities to spend their efforts on providing charitable services.
+                </p>
+              </div>
+            </div>
+          </main>
+        </div>
+        
+      </div>
+      
     );
   }
 }
